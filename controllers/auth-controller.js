6825f2377d95a6a228b4873a8af442511a0dac3d
ephrim-s <ephrims5@gmail.com>
2025-01-08 10:01:53 +0000

    added change of password to auth-controller

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -92,3 +92,51 @@ export const loginUser = async (req, res) => {
     });
     }
 }; 
+
+export const changePassword = async(req, res)=> {
+    try {
+        const userId = req.userInfo.userId;
+
+        //extract old and new password
+        const {oldPassword, newPassword} = req.body;
+
+        //find the current logged in user
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        //check if the old password is correct
+        const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                success: false,
+                message: 'Old password is not correct! Please try again'
+            });
+        }
+        //hash the new password here 
+        const salt = await bcrypt.genSalt(10);
+        const newHashedPassword = await bcrypt.hash(newPassword, salt);
+
+        //update user password
+        user.password = newHashedPassword;
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: 'Password changed successfully'           
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Somthing went wrong"
+    });
+    }
+};
